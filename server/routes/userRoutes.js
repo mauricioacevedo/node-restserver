2d76dc3cc9a150536d90d3f36c9184a588079b60
Mauricio Acevedo <mauricio.acevedo@gmail.com>
2018-11-27 05:21:19 -0500

    userRoutes.js modified

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
-const User = require('../models/User')
+const User = require('../models/user')
 
 const app = express();
 
